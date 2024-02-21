2ff5b0ff982740bd3eb9d254fb4829935d774f86
Gilberto Pereira <91733426+GilbertoPereira13@users.noreply.github.com>
2024-02-21 11:54:13 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const cacheName = 'site-static-v1.6';
+const cacheName = 'site-static-v1.8';
 const assets = [
-    '/pesquisav1/',
+    '/',
     '/pesquisav1/index.html',
     '/pesquisav1/styles.css',
     '/pesquisav1/script.js',
@@ -10,34 +10,18 @@ const assets = [
     // Adicione outros recursos necessários
 ];
 
-// Cache on install
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(cacheName)
-        .then(cache => {
-            cache.addAll(assets);
-        })
-    );
-});
-
-// Fetch event
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-        .then(response => {
-            return response || fetch(event.request);
-        })
-    );
-});
-
-// Activate event - clean old caches
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(keys
-                .filter(key => key !== cacheName)
-                .map(key => caches.delete(key))
-            );
-        })
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Retorna o recurso do cache se disponível
+        return response || fetch(event.request).then(fetchResponse => {
+          // Se o recurso for buscado com sucesso na rede, adicione-o ao cache
+          return caches.open(cacheName).then(cache => {
+            cache.put(event.request, fetchResponse.clone());
+            return fetchResponse;
+          });
+        });
+      })
+  );
 });
