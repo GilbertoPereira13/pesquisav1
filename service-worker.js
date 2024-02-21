3eb4a3809d783dd7e93a7fc1bcfd0f7823083501
Gilberto Pereira <91733426+GilbertoPereira13@users.noreply.github.com>
2024-02-21 11:02:20 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,19 @@
-const cacheName = 'v1.1';
-const resourcesToPreload = [
-  '/',
-  'index.html',
-  'styles.css',
-  'script.js',
-  'manifest.json',
-  'images/icon-192x192.png',
-  'images/icon-512x512.png'
-  // Adicione outros recursos que você queira armazenar em cache
+const cacheName = 'site-static-v1.3';
+const assets = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/script.js',
+    'manifest.json',
+    '/pesquisav1/images/icon-192x192.png',
+    '/pesquisav1/images/icon-512x512.png',
+    // Adicione outros recursos necessários
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(cacheName)
-      .then(cache => cache.addAll(resourcesToPreload))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(cachedResponse => {
-          return cachedResponse || fetch(event.request);
-      })
-  );
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(cacheName).then(cache => {
+            return cache.addAll(assets).catch(error => console.log('Falha ao cache assets:', error));
+        })
+    );
 });
